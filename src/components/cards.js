6b1b7e964133f95bf90e02dbb4d128bc99fa2ae4
M-Dan814 <m.dayan5689@gmail.com>
2023-01-21 20:02:25 -0800

    Create array for rendering item cards

---
+++ src/components/cards.js
@@ -0,0 +1,64 @@
+const cards = [
+  {
+    name: "Apples",
+    img: require("./images/apples.jpg"),
+    price: 0.2,
+  },
+  {
+    name: "Strawberries",
+    img: require("./images/strawberry.jpg"),
+    price: 0.35,
+  },
+  {
+    name: "Pineapples",
+    img: require("./images/pineapples.jpg"),
+    price: 0.75,
+  },
+  {
+    name: "Peaches",
+    img: require("./images/peaches.jpg"),
+    price: 0.25,
+  },
+  {
+    name: "Oranges",
+    img: require("./images/Oranges.jpeg"),
+    price: 0.3,
+  },
+  {
+    name: "Tomatoes",
+    img: require("./images/tomatoes.jpg"),
+    price: 0.25,
+  },
+  {
+    name: "Watermelons",
+    img: require("./images/watermelon.jpg"),
+    price: 1.5,
+  },
+  {
+    name: "Melons",
+    img: require("./images/melon.jpg"),
+    price: 1.2,
+  },
+  {
+    name: "Lemons",
+    img: require("./images/lemons.jpg"),
+    price: 0.1,
+  },
+  {
+    name: "Grapes",
+    img: require("./images/grapes.jpg"),
+    price: 0.85,
+  },
+  {
+    name: "Bananas",
+    img: require("./images/bananas.jpg"),
+    price: 1.25,
+  },
+  {
+    name: "Mangoes",
+    img: require("./images/mangoes.jpg"),
+    price: 0.45,
+  },
+];
+
+export { cards };
